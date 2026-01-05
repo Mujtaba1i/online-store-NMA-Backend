fc3ef98e4948639bded5d10fbd4c2e39f06bce2d
abdulrahman-alhattab <osmiumh30@gmail.com>
2026-01-05 10:30:16 +0300

    help

---
+++ models/product.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+// we need mongoose schema
+const productSchema = mongoose.Schema(
+{
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    stock: {
+      type: Number,
+      required: true
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    }
+  },
+  {
+    Timestamps: true
+  }
+)
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    delete returnedObject.password
+  }
+})
+
+// then we register the model with mongoose
+const product = mongoose.model('product', productSchema)
+
+// export the model
+module.exports = product
